542444fa6a81a5aa474b052a0f78fe4ce2a5590a
joxiah025 <creativejosiah@gmail.com>
2020-03-08 19:41:25 +0100

    Added projects to the solution file

---
+++ CleanArchitecture.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleanArchitecture.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
